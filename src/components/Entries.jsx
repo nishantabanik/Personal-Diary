bcd3df2c38a3576bce0490864778c1cf2b26ed9f
Johanna <johanna.k.1996@googlemail.com>
2025-02-05 14:05:01 +0100

    fixed loading of local storage, added grid for entries, added modal for viewing entry data

--- src/components/Entries.jsx
+++ src/components/Entries.jsx
@@ -1,13 +1,20 @@
+import React, { useState } from "react";
+import ViewEntryModal from "./ViewEntryModal";
+
 const Entries = ({ entries }) => {
+  // useState for selcting single entry for modal
+  const [selectedEntry, setSelectedEntry] = useState(null);
+
   // Receiving entries as props
   return (
-    <div className="space-y-4">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 my-16">
       {" "}
       {/* Container for entries */}
       {entries.map((entry) => (
         <div
           key={entry.id}
-          className="card bg-base-100 shadow-md p-4 rounded-lg"
+          className="card bg-base-100 shadow-md p-4 rounded-lg cursor-pointer"
+          onClick={() => setSelectedEntry(entry)}
         >
           {" "}
           {/* Entry card */}
@@ -24,6 +31,12 @@ const Entries = ({ entries }) => {
           <p>{entry.content}</p> {/* Entry content */}
         </div>
       ))}
+      {/* View Entry Modal */}
+      <ViewEntryModal
+        isOpen={!!selectedEntry}
+        entry={selectedEntry}
+        onClose={() => setSelectedEntry(null)}
+      />
     </div>
   );
 };
